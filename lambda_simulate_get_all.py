2fb81d53d740a7ce88aa160d157e936208bdf2fa
Lucho <lucianotoniani1987@hotmail.com>
2024-09-03 11:09:03 -0300

    Eventos simulados lambda_post lambda_get_all

--- lambda_simulate_get_all.py
+++ lambda_simulate_get_all.py
@@ -2,11 +2,12 @@ from src.contacts.app import lambda_handler
 import json
 
 # Configura el evento para simular una solicitud GET /contacts
+test_token = "IQoJb3JpZ2luX2VjEKL//////////wEaCWV1LXdlc3QtMSJIMEYCIQCWAdQVbk2Xe9XlzJV+wQrJ9EXRmT0b2UUXbpDa8RrNxwIhAOgQQ3fWy8z+n7r7oWtUedoFYpyEE0DYEqDHpyemsDF6KpgDCLv//////////wEQABoMMjExMTI1NTYxMDA1IgxYD+kJSr87SmNTMbEq7AK+DWhh0StDEvBOGkWog0NF1subOu08iv6t2igrhci3iE400CecIbmGESCQw7xGJvms2xL3bDXhIT4Hb6zeo/MYI4/YY4PJVbILjmLdXcizuGyovZroSRrMgqrLVvvio3dA5D6D5tTskmGToZZHziHyRS2mv6vlCKW4Gd5Lk4fIkN1LgZ4dx+OJdZMQ4/LeJns4zuOKCcH6TUciiKpih6NXBeaDLqHUqWn8G6/4tvKyGx10KaIo0eTvW34MdgkVgqYysfYkNdKvCkN+rISBH3bSjJgugaDOLgs+57Oyh+U2dvZbbGfGzArR3WmtptqZyWK4jNhaWBN1yYTNMJMMfDTxA6UAL72xrjW8j9+patZwftOEGiM44UVFQNpbX/jipha+OWuCAs9b4jzoJAZpuqFXS8DAb/o3MPV9A3uI6xBnm+IeilczMY8ReK1BiJBm0uOZTleKH5Vh5zotUI8k1WQ8o0/lr7+tcNBfh/TvMPi627YGOqUBxcdwX4nau7V7qgYCkpvYZxgy5L1368tXaJEF3VHVhiN09PEt4J98D6zh0rTS+hRtwyr4txLOtZnKFWQHHTbUgfVhQiJ6jrxk+iSvpX4RM1FeLcS92snEdlJiPheUYE+2y+q263plvrqNW3prkNoNc8FVuaXmUjLZVjrZGc3eMxhjh6je2Ut0/DI/E+1J/k/AUyH1N9LukRGCbL5Xk5c6q+5njc/q"
 event = {
     "routeKey": "GET /contacts",
-    "token": "IQoJb3JpZ2luX2VjEKL//////////wEaCWV1LXdlc3QtMSJIMEYCIQCWAdQVbk2Xe9XlzJV+wQrJ9EXRmT0b2UUXbpDa8RrNxwIhAOgQQ3fWy8z+n7r7oWtUedoFYpyEE0DYEqDHpyemsDF6KpgDCLv//////////wEQABoMMjExMTI1NTYxMDA1IgxYD+kJSr87SmNTMbEq7AK+DWhh0StDEvBOGkWog0NF1subOu08iv6t2igrhci3iE400CecIbmGESCQw7xGJvms2xL3bDXhIT4Hb6zeo/MYI4/YY4PJVbILjmLdXcizuGyovZroSRrMgqrLVvvio3dA5D6D5tTskmGToZZHziHyRS2mv6vlCKW4Gd5Lk4fIkN1LgZ4dx+OJdZMQ4/LeJns4zuOKCcH6TUciiKpih6NXBeaDLqHUqWn8G6/4tvKyGx10KaIo0eTvW34MdgkVgqYysfYkNdKvCkN+rISBH3bSjJgugaDOLgs+57Oyh+U2dvZbbGfGzArR3WmtptqZyWK4jNhaWBN1yYTNMJMMfDTxA6UAL72xrjW8j9+patZwftOEGiM44UVFQNpbX/jipha+OWuCAs9b4jzoJAZpuqFXS8DAb/o3MPV9A3uI6xBnm+IeilczMY8ReK1BiJBm0uOZTleKH5Vh5zotUI8k1WQ8o0/lr7+tcNBfh/TvMPi627YGOqUBxcdwX4nau7V7qgYCkpvYZxgy5L1368tXaJEF3VHVhiN09PEt4J98D6zh0rTS+hRtwyr4txLOtZnKFWQHHTbUgfVhQiJ6jrxk+iSvpX4RM1FeLcS92snEdlJiPheUYE+2y+q263plvrqNW3prkNoNc8FVuaXmUjLZVjrZGc3eMxhjh6je2Ut0/DI/E+1J/k/AUyH1N9LukRGCbL5Xk5c6q+5njc/q",
     "headers": {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {test_token}"
     },
     "body": json.dumps({})  # Para un GET, el cuerpo generalmente puede estar vacío
 }
--- src/contacts/app.py
+++ src/contacts/app.py
@@ -8,7 +8,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+dynamodb = boto3.Session(profile_name="211125561005_apser-TEMP-AdministratorAccess").resource('dynamodb', region_name='eu-west-1')
 table = dynamodb.Table("Test_lambda_vero")  # Asegúrate de que este nombre sea correcto
 
 def lambda_handler(event, context):
@@ -18,13 +18,22 @@ def lambda_handler(event, context):
     headers = {"Content-Type": "application/json"}
 
     # Obtener el token de autorización del encabezado
-    auth_header = event.get('token', {})
-    token = auth_header.split(' ')[1] if auth_header and ' ' in auth_header else None
+    auth_header = event.get('headers', {}).get('Authorization', '')
+    print(f"auth_header: {auth_header}")  # Imprime el valor del encabezado
 
+    # Verifica el formato del encabezado y extrae el token
+    if auth_header.startswith('Bearer '):
+        token = auth_header.split(' ')[1]
+    else:
+        token = None
+
+    print(f"Extracted token: {token}")  # Imprime el valor del token extraído
+
+    # Verifica si el token es válido
     if not token or not verify_token(token):
         return {
             'statusCode': 401,
-            'headers': headers,
+            'headers': {"Content-Type": "application/json"},
             'body': json.dumps({'error': 'Unauthorized'})
         }
 
@@ -77,7 +86,7 @@ def lambda_handler(event, context):
     }
 
 def verify_token(token):
-    valid_tokens = ['token', 'another-valid-token']
+    valid_tokens = ['valid-token', 'another-valid-token', 'IQoJb3JpZ2luX2VjEKL//////////wEaCWV1LXdlc3QtMSJIMEYCIQCWAdQVbk2Xe9XlzJV+wQrJ9EXRmT0b2UUXbpDa8RrNxwIhAOgQQ3fWy8z+n7r7oWtUedoFYpyEE0DYEqDHpyemsDF6KpgDCLv//////////wEQABoMMjExMTI1NTYxMDA1IgxYD+kJSr87SmNTMbEq7AK+DWhh0StDEvBOGkWog0NF1subOu08iv6t2igrhci3iE400CecIbmGESCQw7xGJvms2xL3bDXhIT4Hb6zeo/MYI4/YY4PJVbILjmLdXcizuGyovZroSRrMgqrLVvvio3dA5D6D5tTskmGToZZHziHyRS2mv6vlCKW4Gd5Lk4fIkN1LgZ4dx+OJdZMQ4/LeJns4zuOKCcH6TUciiKpih6NXBeaDLqHUqWn8G6/4tvKyGx10KaIo0eTvW34MdgkVgqYysfYkNdKvCkN+rISBH3bSjJgugaDOLgs+57Oyh+U2dvZbbGfGzArR3WmtptqZyWK4jNhaWBN1yYTNMJMMfDTxA6UAL72xrjW8j9+patZwftOEGiM44UVFQNpbX/jipha+OWuCAs9b4jzoJAZpuqFXS8DAb/o3MPV9A3uI6xBnm+IeilczMY8ReK1BiJBm0uOZTleKH5Vh5zotUI8k1WQ8o0/lr7+tcNBfh/TvMPi627YGOqUBxcdwX4nau7V7qgYCkpvYZxgy5L1368tXaJEF3VHVhiN09PEt4J98D6zh0rTS+hRtwyr4txLOtZnKFWQHHTbUgfVhQiJ6jrxk+iSvpX4RM1FeLcS92snEdlJiPheUYE+2y+q263plvrqNW3prkNoNc8FVuaXmUjLZVjrZGc3eMxhjh6je2Ut0/DI/E+1J/k/AUyH1N9LukRGCbL5Xk5c6q+5njc/q']
     return token in valid_tokens
 
 def delete_contact(pk):
